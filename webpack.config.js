372e2de68a0da85b0a55a1b605b46ab369242dc9
sandra <60742092+sandra33660@users.noreply.github.com>
2020-05-26 17:08:11 +0200

    add html webpack template

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const htmlWebpackTemplate = require("html-webpack-template");
 
 module.exports = {
   mode: "production",
@@ -9,9 +11,15 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: "application_recette",
       meta: { viewport: "width=device-width, initial-scale=1.0" },
+      inject: true,
+      template: htmlWebpackTemplate,
+      links: [
+        "https://fonts.googleapis.com/css2?family=Roboto:wght@100;400&display=swap",
+      ],
     }),
   ],
   module: {
