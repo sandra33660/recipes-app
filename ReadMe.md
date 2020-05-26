aa226d04ad545814830b1fe6e44708ad33ddd02e
sandra <60742092+sandra33660@users.noreply.github.com>
2020-05-26 16:29:33 +0200

    commit rectification

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# Recipes App
+
+Install dependencies:
+
+```Shell
+npm install
+```
+
+Run Build:
+
+```Shell
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "application_recette",
+      meta: { viewport: "width=device-width, initial-scale=1.0" },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
